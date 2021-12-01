867fe7cc46960c86de8b63c39aea991eb34c8505
王钰 <yuwang@wangyudembp.lan>
2021-11-30 23:42:12 -0800

    changed greeting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/',(req,res) => {
-    res.send({hi:'there'});
+    res.send({bye:'bro'});
 })
 
 const PORT = process.env.PORT || 4000;
