e50fe8c420a1e44a0326f79e0040fb587f46daf3
王钰 <yuwang@wangyudembp.lan>
2022-01-07 22:01:01 -0800

    finish oath

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/Users');
+require('./services/passport');
+
+
+mongoose.connect(keys.mongoURI);
+
+
 const app = express();
-app.get('/',(req,res) => {
-    res.send({bye:'bro'});
-})
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/auth.Routes')(app);
+
+
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT);
